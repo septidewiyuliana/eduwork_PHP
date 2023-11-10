9c78cbbf202526ccbd57dc5921f6dad3a986e33c
septidewiyuliana <septidewiyuliana@gmail.com>
2023-11-10 17:24:53 +0700

    complete task 29-32

--- sesi29/anggota.php
+++ sesi29/anggota.php
@@ -1,3 +1,4 @@
+
 <?php
 include "data.php";
 $query = mysqli_query($conn, "SELECT * FROM anggota");
@@ -6,10 +7,9 @@ $query = mysqli_query($conn, "SELECT * FROM anggota");
 <!DOCTYPE html>
 <html>
 <head>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/js/bootstrap.min.js"></script>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
  
 </head>
--- sesi29/index.php
+++ sesi29/index.php
@@ -7,8 +7,8 @@ $query = mysqli_query($conn, "SELECT * FROM buku");
 <html>
 <head>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 <body>
